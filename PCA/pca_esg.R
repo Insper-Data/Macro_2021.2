303e50f7cd867607a3347dc3ec7f5233ca40984f
enzotakeshi <takeshi.takahashi@hotmail.com>
2021-09-14 17:40:31 -0300

    pca esg

--- PCA/pca_esg.R
+++ PCA/pca_esg.R
@@ -1,41 +1,43 @@
+setwd("~/Insper/Data/Projeto Macro/")
+
 rm(list=ls())
 
 library(readxl)
 library(tidyverse)
 library(factoextra)
 library(ggrepel)
+library(plm)
+library(dplyr)
 
-data <- read_excel("~/Insper/Data/Projeto Macro/full.xlsx")
+# Extraindo e arrumando os dados
+
+data <- read_excel("ESG_database.xlsx")
 data <- data[,-c(2)]
 
 panel_data <- pdata.frame(data, index = c("Country.Name", "Time"))
+panel_data <- panel_data[,-c(1,2)]
 
-# Tirando as colunas que não vão ser utilizadas (mobile também)
-panel_data <- data[,-c(1,2,21)]
+# PCA
 
-pca <- panel_data %>%
+## Ajustando escala dos dados
+## Inverter o sinal para interpretabilidade
+
+pca <- -panel_data %>%
   prcomp(scale = TRUE)
 
-# proportion of variance explained
-(pve <- cumsum(pca$sdev^2) / sum(pca$sdev^2))
+## Scree plot
 
 fviz_eig(pca, addlabels = TRUE) +  # scree plot
   xlab("Componente Principal") +
-  ylab("ProporÃ§Ã£o explicada da variÃ¢ncia")
+  ylab("Proporção Explicada da Variância")
 
-# Kaiser, Henry F. 1961. "A Note on Guttman's Lower Bound for the Number of Common Factors." British Journal of Statistical Psychology 14: 1-2.
-# An eigenvalue > 1 indicates that PCs account for more variance than accounted by one of the original variables in standardized data. This is commonly used as a cutoff point for which PCs are retained. This holds true only when the data are standardized.
-get_eigenvalue(pca)[1:10, 1:2]
-# equivalente a
-(pca$sdev^2)[1:10]
+## Cargas
 
 Phi <- pca$rotation
 
-# a contribuiÃ§Ã£o percentual de uma variÃ¡vel Ã© 100 * o quadrado
-# da carga correspodente dividido pela soma dos quadrados das cargas
-sort(100 * Phi[, 1]^2 / sum(Phi[, 1]^2), decreasing = TRUE)
-# a linha tracejada seria o valor de uma contribuiÃ§Ã£o percentual
-# igual para cada variÃ¡vel; ou seja, 100 / nÃºmero de variÃ¡veis
+# View(Phi)
+
+## Contribuições para as PCs
 
 pca %>% fviz_contrib(choice = "var", axes = 1, sort.val = "asc", fill = "steelblue", color = "black") + 
   labs(x = "", title = "Contribuiçõess das variáveis para a PC1") + 
@@ -49,10 +51,5 @@ pca %>% fviz_contrib(choice = "var", axes = 3, sort.val = "asc", fill = "steelbl
   labs(x = "", title = "Contribuiçõess das variáveis para a PC3") + 
   coord_flip()
 
-pca %>% fviz_contrib(choice = "var", axes = 4, sort.val = "asc", fill = "steelblue", color = "black") + 
-  labs(x = "", title = "Contribuiçõess das variáveis para a PC4") + 
-  coord_flip()
 
-pca %>% fviz_contrib(choice = "var", axes = 5, sort.val = "asc", fill = "steelblue", color = "black") + 
-  labs(x = "", title = "Contribuiçõess das variáveis para a PC5") + 
-  coord_flip()
+
