77fb1f7fa572118a312436779d7fb29ac5649c61
enzotakeshi <takeshi.takahashi@hotmail.com>
2021-11-02 20:07:15 -0300

    lasso + lag

--- LASSO/lags.R
+++ LASSO/lags.R
@@ -1,3 +1,7 @@
+setwd("~/Insper/Data/Projeto Macro/Data_Macro_ESG/Macro_2021.2")
+
+rm(list=ls())
+
 library(tidyverse)
 library(transformr)
 library(stringi)
@@ -5,97 +9,108 @@ library(skimr)
 library(stargazer)
 library(plm)
 library(lmtest)
+library(glmnet)
 
-rm(list=ls())
-dataset_total_jan_2021 <- read.csv("~/Insper/Data/Projeto Macro/Data_Macro_ESG/Macro_2021.2/Bases/merged.csv")
+dataset_total_jan_2021 <- read.csv("Bases/merged.csv")
 
 panel_dataset <- pdata.frame(dataset_total_jan_2021, index = c("country", "year"))
 
-# Panel dataset 1 lag added
+# Creating LAGS + MEAN
+
+# Panel dataset - 1 lag added
 
 panel_dataset_lags <- panel_dataset %>% 
   group_by(country) %>% 
   mutate(
+    
+    # Adding GDP Growth
+    GDP_growth = 100*(GDP_cur - dplyr::lag(GDP_cur,1))/dplyr::lag(GDP_cur,1),
+    # Adding foreign debt to GDP
+    foreign_debt_to_gdp = foreign_debt / GDP_cur,
+    # Lags
+    lag_GDP_growth_1 = dplyr::lag(GDP_growth,1),
+    lag_fx_volatility_1 = dplyr::lag(fx_volatility,1),
+    lag_GDP_per_cap_cur_USD_1 = dplyr::lag(GDP_per_cap_cur_USD,1),
+    lag_nominal_rate_1 = dplyr::lag(nominal_rate,1),
+    lag_taxes_1 = dplyr::lag(taxes,1),
+    lag_ln_GDP_per_cap_cte_1 = dplyr::lag(ln_GDP_per_cap_cte,1),
+    lag_account_balance_1 = dplyr::lag(account_balance,1),
+    lag_lending_borroeing_rate_1 = dplyr::lag(lending_borroeing_rate,1),
+    lag_unemployment_1 = dplyr::lag(unemployment,1),
+    lag_inflation_mean_1 = dplyr::lag(inflation_mean,1),
+    lag_debt_to_GDP_1 = dplyr::lag(debt_to_GDP,1),
+    lag_real_interest_rate_1 = dplyr::lag(real_interest_rate,1),
+    lag_ESGIp_1 = dplyr::lag(ESGIp,1),
+    lag_Ep_1 = dplyr::lag(Ep,1),
+    lag_Sp_1 = dplyr::lag(Sp,1),
+    lag_Gp_1 = dplyr::lag(Gp,1),
+    lag_foreign_debt_to_gdp_1 = dplyr::lag(foreign_debt_to_gdp, 1),
+    lag_vix_EUA_1 = dplyr::lag(vix_EUA, 1))
 
-GDP_growth = 100*(GDP_cur - dplyr::lag(GDP_cur,1))/dplyr::lag(GDP_cur,1),
-foreign_debt_to_gdp = foreign_debt / GDP_cur,
-lag_GDP_growth_1 = dplyr::lag(GDP_growth,1),
-lag_fx_volatility_1 = dplyr::lag(fx_volatility,1),
-lag_GDP_per_cap_cur_USD_1 = dplyr::lag(GDP_per_cap_cur_USD,1),
-lag_nominal_rate_1 = dplyr::lag(nominal_rate,1),
-lag_taxes_1 = dplyr::lag(taxes,1),
-lag_ln_GDP_per_cap_cte_1 = dplyr::lag(ln_GDP_per_cap_cte,1),
-lag_account_balance_1 = dplyr::lag(account_balance,1),
-lag_lending_borroeing_rate_1 = dplyr::lag(lending_borroeing_rate,1),
-lag_unemployment_1 = dplyr::lag(unemployment,1),
-lag_inflation_mean_1 = dplyr::lag(inflation_mean,1),
-lag_debt_to_GDP_1 = dplyr::lag(debt_to_GDP,1),
-lag_real_interest_rate_1 = dplyr::lag(real_interest_rate,1),
-lag_ESGIp_1 = dplyr::lag(ESGIp,1),
-lag_Ep_1 = dplyr::lag(Ep,1),
-lag_Sp_1 = dplyr::lag(Sp,1),
-lag_Gp_1 = dplyr::lag(Gp,1),
-lag_foreign_debt_to_gdp_1 = dplyr::lag(foreign_debt_to_gdp, 1),
-lag_vix_EUA_1 = dplyr::lag(vix_EUA, 1))
 
-
-# Panel dataset 2 lag added
+# Panel dataset - 2 lag added
 
 panel_dataset_lags_2 <- panel_dataset %>%
   group_by(country) %>% 
-mutate(
+  mutate(
 
-GDP_growth = 100*(GDP_cur - dplyr::lag(GDP_cur,1))/dplyr::lag(GDP_cur,1),
-foreign_debt_to_gdp = foreign_debt / GDP_cur,
-lag_GDP_growth_2 = dplyr::lag(GDP_growth,2),    
-GDP_growth_2 = 100*(GDP_cur - dplyr::lag(GDP_cur,2))/dplyr::lag(GDP_cur,2),
-lag_fx_volatility_2 = dplyr::lag(fx_volatility,2),
-lag_GDP_per_cap_cur_USD_2 = dplyr::lag(GDP_per_cap_cur_USD,2),
-lag_nominal_rate_2 = dplyr::lag(nominal_rate,2),
-lag_taxes_2 = dplyr::lag(taxes,2),
-lag_ln_GDP_per_cap_cte_2 = dplyr::lag(ln_GDP_per_cap_cte,2),
-lag_account_balance_2 = dplyr::lag(account_balance,2),
-lag_lending_borroeing_rate_2 = dplyr::lag(lending_borroeing_rate,2),
-lag_unemployment_2 = dplyr::lag(unemployment,2),
-lag_inflation_mean_2 = dplyr::lag(inflation_mean,2),
-lag_debt_to_GDP_2 = dplyr::lag(debt_to_GDP,2),
-lag_real_interest_rate_2 = dplyr::lag(real_interest_rate,2),
-lag_ESGIp_2 = dplyr::lag(ESGIp,2),
-lag_Ep_2 = dplyr::lag(Ep,2),
-lag_Sp_2 = dplyr::lag(Sp,2),
-lag_Gp_2 = dplyr::lag(Gp,2),
-lag_foreign_debt_to_gdp_2 = dplyr::lag(foreign_debt_to_gdp, 2),
-lag_vix_EUA_2 = dplyr::lag(vix_EUA, 2))
+    # Adding GDP Growth
+    GDP_growth = 100*(GDP_cur - dplyr::lag(GDP_cur,1))/dplyr::lag(GDP_cur,1),
+    # Adding foreign debt to GDP
+    foreign_debt_to_gdp = foreign_debt / GDP_cur,
+    # Lags
+    lag_GDP_growth_2 = dplyr::lag(GDP_growth,2),    
+    GDP_growth_2 = 100*(GDP_cur - dplyr::lag(GDP_cur,2))/dplyr::lag(GDP_cur,2),
+    lag_fx_volatility_2 = dplyr::lag(fx_volatility,2),
+    lag_GDP_per_cap_cur_USD_2 = dplyr::lag(GDP_per_cap_cur_USD,2),
+    lag_nominal_rate_2 = dplyr::lag(nominal_rate,2),
+    lag_taxes_2 = dplyr::lag(taxes,2),
+    lag_ln_GDP_per_cap_cte_2 = dplyr::lag(ln_GDP_per_cap_cte,2),
+    lag_account_balance_2 = dplyr::lag(account_balance,2),
+    lag_lending_borroeing_rate_2 = dplyr::lag(lending_borroeing_rate,2),
+    lag_unemployment_2 = dplyr::lag(unemployment,2),
+    lag_inflation_mean_2 = dplyr::lag(inflation_mean,2),
+    lag_debt_to_GDP_2 = dplyr::lag(debt_to_GDP,2),
+    lag_real_interest_rate_2 = dplyr::lag(real_interest_rate,2),
+    lag_ESGIp_2 = dplyr::lag(ESGIp,2),
+    lag_Ep_2 = dplyr::lag(Ep,2),
+    lag_Sp_2 = dplyr::lag(Sp,2),
+    lag_Gp_2 = dplyr::lag(Gp,2),
+    lag_foreign_debt_to_gdp_2 = dplyr::lag(foreign_debt_to_gdp, 2),
+    lag_vix_EUA_2 = dplyr::lag(vix_EUA, 2))
 
 
 
-# Panel dataset 3 lag added
+# Panel dataset - 3 lag added
 
 panel_dataset_lags_3 <- panel_dataset %>% 
   group_by(country) %>% 
   mutate(
 
-GDP_growth = 100*(GDP_cur - dplyr::lag(GDP_cur,1))/dplyr::lag(GDP_cur,1),
-foreign_debt_to_gdp = foreign_debt / GDP_cur,
-lag_GDP_growth_3 = dplyr::lag(GDP_growth,3),    
-GDP_growth_3 = 100*(GDP_cur - dplyr::lag(GDP_cur,3))/dplyr::lag(GDP_cur,3),
-lag_fx_volatility_3 = dplyr::lag(fx_volatility,3),
-lag_GDP_per_cap_cur_USD_3 = dplyr::lag(GDP_per_cap_cur_USD,3),
-lag_nominal_rate_3 = dplyr::lag(nominal_rate,3),
-lag_taxes_3 = dplyr::lag(taxes,3),
-lag_ln_GDP_per_cap_cte_3 = dplyr::lag(ln_GDP_per_cap_cte,3),
-lag_account_balance_3 = dplyr::lag(account_balance,3),
-lag_lending_borroeing_rate_3 = dplyr::lag(lending_borroeing_rate,3),
-lag_unemployment_3 = dplyr::lag(unemployment,3),
-lag_inflation_mean_3 = dplyr::lag(inflation_mean,3),
-lag_debt_to_GDP_3 = dplyr::lag(debt_to_GDP,3),
-lag_real_interest_rate_3 = dplyr::lag(real_interest_rate,3),
-lag_ESGIp_3 = dplyr::lag(ESGIp,3),
-lag_Ep_3 = dplyr::lag(Ep,3),
-lag_Sp_3 = dplyr::lag(Sp,3),
-lag_Gp_3 = dplyr::lag(Gp,3),
-lag_foreign_debt_to_gdp_3 = dplyr::lag(foreign_debt_to_gdp, 3),
-lag_vix_EUA_3 = dplyr::lag(vix_EUA, 3))
+    # Adding GDP Growth
+    GDP_growth = 100*(GDP_cur - dplyr::lag(GDP_cur,1))/dplyr::lag(GDP_cur,1),
+    # Adding foreign debt to GDP
+    foreign_debt_to_gdp = foreign_debt / GDP_cur,
+    # Lags
+    lag_GDP_growth_3 = dplyr::lag(GDP_growth,3),    
+    GDP_growth_3 = 100*(GDP_cur - dplyr::lag(GDP_cur,3))/dplyr::lag(GDP_cur,3),
+    lag_fx_volatility_3 = dplyr::lag(fx_volatility,3),
+    lag_GDP_per_cap_cur_USD_3 = dplyr::lag(GDP_per_cap_cur_USD,3),
+    lag_nominal_rate_3 = dplyr::lag(nominal_rate,3),
+    lag_taxes_3 = dplyr::lag(taxes,3),
+    lag_ln_GDP_per_cap_cte_3 = dplyr::lag(ln_GDP_per_cap_cte,3),
+    lag_account_balance_3 = dplyr::lag(account_balance,3),
+    lag_lending_borroeing_rate_3 = dplyr::lag(lending_borroeing_rate,3),
+    lag_unemployment_3 = dplyr::lag(unemployment,3),
+    lag_inflation_mean_3 = dplyr::lag(inflation_mean,3),
+    lag_debt_to_GDP_3 = dplyr::lag(debt_to_GDP,3),
+    lag_real_interest_rate_3 = dplyr::lag(real_interest_rate,3),
+    lag_ESGIp_3 = dplyr::lag(ESGIp,3),
+    lag_Ep_3 = dplyr::lag(Ep,3),
+    lag_Sp_3 = dplyr::lag(Sp,3),
+    lag_Gp_3 = dplyr::lag(Gp,3),
+    lag_foreign_debt_to_gdp_3 = dplyr::lag(foreign_debt_to_gdp, 3),
+    lag_vix_EUA_3 = dplyr::lag(vix_EUA, 3))
 
 
 
@@ -105,28 +120,33 @@ panel_dataset_mean_lags <- panel_dataset %>%
   group_by(country) %>% 
   mutate(
 
-GDP_growth = 100*(GDP_cur - dplyr::lag(GDP_cur,1))/dplyr::lag(GDP_cur,1),
-foreign_debt_to_gdp = foreign_debt / GDP_cur,
-lag_GDP_growth_mean = (dplyr::lag(GDP_growth,1) + dplyr::lag(GDP_growth,2) + dplyr::lag(GDP_growth,3))/3, 
-lag_fx_volatility_mean = (dplyr::lag(fx_volatility,1) +dplyr::lag(fx_volatility,2) +dplyr::lag(fx_volatility,3))/3,
-lag_GDP_per_cap_cur_USD_mean = (dplyr::lag(GDP_per_cap_cur_USD,1) +dplyr::lag(GDP_per_cap_cur_USD,2)+dplyr::lag(GDP_per_cap_cur_USD,3))/3,
-lag_nominal_rate_mean = (dplyr::lag(nominal_rate,1)+dplyr::lag(nominal_rate,2)+dplyr::lag(nominal_rate,3))/3,
-lag_taxes_mean = (dplyr::lag(taxes,1)+dplyr::lag(taxes,2)+dplyr::lag(taxes,3))/3,
-lag_ln_GDP_per_cap_cte_mean = (dplyr::lag(ln_GDP_per_cap_cte,1)+dplyr::lag(ln_GDP_per_cap_cte,2)+dplyr::lag(ln_GDP_per_cap_cte,3))/3,
-lag_account_balance_mean = (dplyr::lag(account_balance,1)+dplyr::lag(account_balance,2)+dplyr::lag(account_balance,3))/3,
-lag_lending_borroeing_rate_mean = (dplyr::lag(lending_borroeing_rate,1)+dplyr::lag(lending_borroeing_rate,2)+dplyr::lag(lending_borroeing_rate,3))/3,
-lag_unemployment_mean = (dplyr::lag(unemployment,1)+dplyr::lag(unemployment,2)+dplyr::lag(unemployment,3))/3,
-lag_inflation_mean_mean = (dplyr::lag(inflation_mean,1)+dplyr::lag(inflation_mean,2)+dplyr::lag(inflation_mean,3))/3,
-lag_debt_to_GDP_mean = (dplyr::lag(debt_to_GDP,1)+dplyr::lag(debt_to_GDP,2)+dplyr::lag(debt_to_GDP,3))/3,
-lag_real_interest_rate_mean = (dplyr::lag(real_interest_rate,1)+dplyr::lag(real_interest_rate,2)+dplyr::lag(real_interest_rate,3))/3,
-lag_ESGIp_mean = (dplyr::lag(ESGIp,1)+dplyr::lag(ESGIp,2)+dplyr::lag(ESGIp,3))/3,
-lag_Ep_mean = (dplyr::lag(Ep,1)+dplyr::lag(Ep,2)+dplyr::lag(Ep,3))/3,
-lag_Sp_mean = (dplyr::lag(Sp,1)+dplyr::lag(Sp,2)+dplyr::lag(Sp,3))/3,
-lag_Gp_mean = (dplyr::lag(Gp,1)+dplyr::lag(Gp,2)+dplyr::lag(Gp,3))/3,
-lag_foreign_debt_to_gdp_mean = (dplyr::lag(foreign_debt_to_gdp, 1) +dplyr::lag(foreign_debt_to_gdp, 2) + dplyr::lag(foreign_debt_to_gdp, 3))/3,
-lag_vix_EUA_mean = (dplyr::lag(vix_EUA, 1) + dplyr::lag(vix_EUA, 2) + dplyr::lag(vix_EUA, 3))/ 3)
+    # Adding GDP Growth
+    GDP_growth = 100*(GDP_cur - dplyr::lag(GDP_cur,1))/dplyr::lag(GDP_cur,1),
+    # Adding foreign debt to GDP
+    foreign_debt_to_gdp = foreign_debt / GDP_cur,
+    # Lags
+    lag_GDP_growth_mean = (dplyr::lag(GDP_growth,1) + dplyr::lag(GDP_growth,2) + dplyr::lag(GDP_growth,3))/3, 
+    lag_fx_volatility_mean = (dplyr::lag(fx_volatility,1) +dplyr::lag(fx_volatility,2) +dplyr::lag(fx_volatility,3))/3,
+    lag_GDP_per_cap_cur_USD_mean = (dplyr::lag(GDP_per_cap_cur_USD,1) +dplyr::lag(GDP_per_cap_cur_USD,2)+dplyr::lag(GDP_per_cap_cur_USD,3))/3,
+    lag_nominal_rate_mean = (dplyr::lag(nominal_rate,1)+dplyr::lag(nominal_rate,2)+dplyr::lag(nominal_rate,3))/3,
+    lag_taxes_mean = (dplyr::lag(taxes,1)+dplyr::lag(taxes,2)+dplyr::lag(taxes,3))/3,
+    lag_ln_GDP_per_cap_cte_mean = (dplyr::lag(ln_GDP_per_cap_cte,1)+dplyr::lag(ln_GDP_per_cap_cte,2)+dplyr::lag(ln_GDP_per_cap_cte,3))/3,
+    lag_account_balance_mean = (dplyr::lag(account_balance,1)+dplyr::lag(account_balance,2)+dplyr::lag(account_balance,3))/3,
+    lag_lending_borroeing_rate_mean = (dplyr::lag(lending_borroeing_rate,1)+dplyr::lag(lending_borroeing_rate,2)+dplyr::lag(lending_borroeing_rate,3))/3,
+    lag_unemployment_mean = (dplyr::lag(unemployment,1)+dplyr::lag(unemployment,2)+dplyr::lag(unemployment,3))/3,
+    lag_inflation_mean_mean = (dplyr::lag(inflation_mean,1)+dplyr::lag(inflation_mean,2)+dplyr::lag(inflation_mean,3))/3,
+    lag_debt_to_GDP_mean = (dplyr::lag(debt_to_GDP,1)+dplyr::lag(debt_to_GDP,2)+dplyr::lag(debt_to_GDP,3))/3,
+    lag_real_interest_rate_mean = (dplyr::lag(real_interest_rate,1)+dplyr::lag(real_interest_rate,2)+dplyr::lag(real_interest_rate,3))/3,
+    lag_ESGIp_mean = (dplyr::lag(ESGIp,1)+dplyr::lag(ESGIp,2)+dplyr::lag(ESGIp,3))/3,
+    lag_Ep_mean = (dplyr::lag(Ep,1)+dplyr::lag(Ep,2)+dplyr::lag(Ep,3))/3,
+    lag_Sp_mean = (dplyr::lag(Sp,1)+dplyr::lag(Sp,2)+dplyr::lag(Sp,3))/3,
+    lag_Gp_mean = (dplyr::lag(Gp,1)+dplyr::lag(Gp,2)+dplyr::lag(Gp,3))/3,
+    lag_foreign_debt_to_gdp_mean = (dplyr::lag(foreign_debt_to_gdp, 1) +dplyr::lag(foreign_debt_to_gdp, 2) + dplyr::lag(foreign_debt_to_gdp, 3))/3,
+    lag_vix_EUA_mean = (dplyr::lag(vix_EUA, 1) + dplyr::lag(vix_EUA, 2) + dplyr::lag(vix_EUA, 3))/ 3)
 
 # LASSO
+
+lambda <- 0.1
 
 # Lag 1
 
@@ -140,7 +160,6 @@ y <- lag_1[, c("spreads")]
 #---------------
 # Model
 #---------------
-lambda <- 0.1
 
 # lasso
 la.eq <- glmnet(X, y, lambda=lambda, 
@@ -150,10 +169,10 @@ la.eq <- glmnet(X, y, lambda=lambda,
 #---------------
 # Results (lambda=0.1)
 #---------------
-df.comp <- data.frame(
+df1.comp <- data.frame(
   Lasso   = la.eq$beta[,1]
 )
-df.comp
+df1.comp
 
 # Lag 2
 
@@ -167,7 +186,6 @@ y <- lag_2[, c("spreads")]
 #---------------
 # Model
 #---------------
-lambda <- 0.1
 
 # lasso
 la.eq <- glmnet(X, y, lambda=lambda, 
@@ -177,11 +195,10 @@ la.eq <- glmnet(X, y, lambda=lambda,
 #---------------
 # Results (lambda=0.1)
 #---------------
-df.comp <- data.frame(
+df2.comp <- data.frame(
   Lasso   = la.eq$beta[,1]
 )
-df.comp
-View(df.comp)
+df2.comp
 
 # Lag 3
 
@@ -195,7 +212,6 @@ y <- lag_3[, c("spreads")]
 #---------------
 # Model
 #---------------
-lambda <- 0.1
 
 # lasso
 la.eq <- glmnet(X, y, lambda=lambda, 
@@ -205,11 +221,10 @@ la.eq <- glmnet(X, y, lambda=lambda,
 #---------------
 # Results (lambda=0.1)
 #---------------
-df.comp <- data.frame(
+df3.comp <- data.frame(
   Lasso   = la.eq$beta[,1]
 )
-df.comp
-View(df.comp)
+df3.comp
 
 # mean
 
@@ -223,7 +238,6 @@ y <- mean[, c("spreads")]
 #---------------
 # Model
 #---------------
-lambda <- 0.1
 
 # lasso
 la.eq <- glmnet(X, y, lambda=lambda, 
@@ -233,8 +247,8 @@ la.eq <- glmnet(X, y, lambda=lambda,
 #---------------
 # Results (lambda=0.1)
 #---------------
-df.comp <- data.frame(
+df_mean.comp <- data.frame(
   Lasso   = la.eq$beta[,1]
 )
-df.comp
-View(df.comp)
+df_mean.comp
+
